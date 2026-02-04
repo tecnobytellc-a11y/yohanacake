7f11993c653d291847efe643ed0b7444cfbb3ecd
tecnobytellc-a11y <tecnobytellc@gmail.com>
2026-02-04 12:12:17 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// Agregamos esta línea para cargar los estilos:
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
